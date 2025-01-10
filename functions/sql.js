8e23e602403baba32545cf6f3cb9842e1d345910
jk <865100300@qq.com>
2025-01-10 15:23:21 +0800

    5

--- functions/sql.js
+++ functions/sql.js
@@ -1,4 +1,5 @@
 export async function onRequest(context) {
+    return new Response('sqls', { status: 400 });
     // Create a prepared statement with our query
     const ps = context.env.NORTHWIND_DB.prepare("SELECT * from home_add_url");
     const data = await ps.first();
